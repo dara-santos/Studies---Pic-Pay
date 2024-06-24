1a45ed80bcc07533f0ff8c6723eb71a468ebc6be
Tatiana Dara <tatiana.santos@picpaybank.com>
2024-06-24 15:41:53 -0300

    Subindo o projeto

---
+++ dara-demo/src/test/java/com/example/dara_demo/DaraDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dara_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaraDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
